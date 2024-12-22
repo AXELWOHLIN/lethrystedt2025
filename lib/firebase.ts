0cd222923701b21510501a9d22b63cbe600824f4
AXELWOHLIN <axelwohlin@gmail.com>
2024-12-22 11:42:05 +0100

    Added firebase keys

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBj2Ka2pO06MWXqRKD5hRSTY4CpF1rcYaU",//process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "mathilda2-a7c48.firebaseapp.com",//process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: "mathilda2-a7c48",//process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: "mathilda2-a7c48.appspot.com",//process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "91082222005",//process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: "1:91082222005:web:18c8ee817390b7501391fc",//process.env.NEXT_PUBLIC_FIREBASE_APP_ID, 
+  measurementId: "G-J8DW7VG2D4"//process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
